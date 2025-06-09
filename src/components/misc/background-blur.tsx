5f228ea722aa3c7426ad8cce543d5eaf391cdadd
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2025-06-09 15:16:09 -0500

    fix: code restructure

---
+++ src/components/misc/background-blur.tsx
@@ -0,0 +1,14 @@
+export const BackgroundBlur = () => {
+  return (
+    <section className="h-screen w-full fixed top-0 left-0 -z-50 flex justify-center items-center bg-black">
+      <section className="max-w-[800px] w-full flex flex-wrap justify-center items-center blur-[100px]">
+        <div className="w-[400px] h-[400px] bg-red-500/40"></div>
+        <div className="w-[400px] h-[400px] bg-yellow-300/40"></div>
+        <div className="w-[400px] h-[400px] bg-green-300/40"></div>
+      </section>
+      <section className="absolute flex justify-center items-center blur-[100px]">
+        <div className="w-[500px] h-[500px] bg-black absolute"></div>
+      </section>
+    </section>
+  );
+};
